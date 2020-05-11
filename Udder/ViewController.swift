69912a67fa0fdf964c793d23654cf4d37c6e47e9
Jurate Kazlauskaite <kazlauskaite.jura@gmail.com>
2020-05-11 11:12:45 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jurate Kazlauskaite on 11/05/2020.
+//  Copyright © 2020 Jurate Kazlauskaite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
