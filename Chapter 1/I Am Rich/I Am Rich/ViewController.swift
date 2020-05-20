1389777f69902ef606547318b09efd64a156c1c3
kazlauskaite <63017946+kazlauskaite@users.noreply.github.com>
2020-05-20 12:39:59 +0100

    Add files via upload

---
+++ Chapter 1/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jurate Kazlauskaite on 01/04/2020.
+//  Copyright © 2020 Jurate Kazlauskaite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
